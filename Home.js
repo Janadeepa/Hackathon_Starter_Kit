716a7fae40b155799e452160eb78e819c3009b55
Yuthmika Janadeepa Priyankara <136349851+Janadeepa@users.noreply.github.com>
2024-05-09 10:05:15 +0530

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <main>
+      <h2>Welcome to the Hackathon Starter Kit!</h2>
+      <p>This repository provides a boilerplate code and project structure to help you get started with your hackathon project.</p>
+    </main>
+  );
+}
+
+export default Home;
