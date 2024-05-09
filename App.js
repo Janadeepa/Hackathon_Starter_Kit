eab42f850618c84953aef9f937a3f2420e424d24
Yuthmika Janadeepa Priyankara <136349851+Janadeepa@users.noreply.github.com>
2024-05-09 10:03:42 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
